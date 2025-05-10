09cc03bef0fb651b55ad79adb60a918d8089749c
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-05-11 00:14:23 +0530

    authorisation middleware set

---
+++ src/middlewares/rbac.middleware.ts
@@ -0,0 +1,18 @@
+import { asyncHandler, ApiError, ErrorCode } from "../utils";
+import { User } from '../models/user.model.js';
+import { Request, Response, NextFunction } from "express";
+
+const rbac = async (...authorise: string[]) => {
+  return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await User.findById(req?.user?._id);
+    if (!user)
+      throw new ApiError(401, 'no user found', ErrorCode.DATA_NOT_FOUND_ERROR);
+
+    if (!authorise.includes(user.role))
+      throw new ApiError(401, "You Are not Authorised for this endpoint", ErrorCode.UNAUTHORIZED);
+
+    if (req.user)
+      req.user.role = user.role;
+    next();
+  })
+};
