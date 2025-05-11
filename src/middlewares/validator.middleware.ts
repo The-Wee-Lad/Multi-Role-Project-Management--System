d972ff18c21cda5bcce01fceebc0fce8321cf3fd
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-05-11 15:23:25 +0530

    validator middleware made

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,2 +1,13 @@
-// TODO:
+import { NextFunction, Request, Response } from "express";
+import { ApiError, asyncHandler, ErrorCode, formatJoiErrors } from "../utils/index.js";
+import Joi from "joi";
+
+const validator = (validationSchema: Joi.ObjectSchema) => {
+  return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { error, value } = validationSchema.validate(req.body)
+    if (error)
+      throw new ApiError(400, "Something is a miss in input, validation error", ErrorCode.VALIDATION_ERROR, formatJoiErrors(error));
+    next();
+  });
+}
 
